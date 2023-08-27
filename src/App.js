bae47b594f638d972ed7f6b1028088724f620b10
Neha <nehatualea@gmail.com>
2023-08-27 17:42:25 +0530

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM  from 'react-dom/client'
+import Header from './components/Header/Header';
+
+const App=()=>{
+    return (
+        <>
+        <Header />
+        </>
+    )
+}
+
+export default App;
