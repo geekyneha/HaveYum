669acd0dac7afb15fce18a9f11b849b4ecb94e71
Neha <nehatualea@gmail.com>
2023-08-30 03:13:34 +0530

    added : shimmer UI

---
+++ src/components/Restaurant/ResCardShimmer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import style from "./res-card-shimmer.module.css";
+const ResCardShimmer = () => {
+  return (
+    <div className={style.card}>
+      <div className={style["image-container"]}>
+        <div className={style.food}></div>
+      </div>
+      <div className={style.content}>
+        <div className={style.restaurant}></div>
+        <div className={style.cuisines}></div>
+
+        <div className={style["max-safety"]}></div>
+      </div>
+    </div>
+  );
+};
+
+export default ResCardShimmer;
