2c47255a8d23993f3c4c6bc88e3a357f94e2f0c1
Neha <nehatualea@gmail.com>
2023-08-30 03:14:25 +0530

    Updated: data

--- src/components/Restaurant/ResCard.js
+++ src/components/Restaurant/ResCard.js
@@ -1,23 +1,35 @@
 import React from "react";
 import style from "./res-card.module.css";
+import { CDN_URL } from "../../utils/constants";
 
-const ResCard = () => {
+const ResCard = ({ resData }) => {
+  const {
+    name,
+    cuisines,
+    deliveryTime,
+    avgRating,
+    costForTwo,
+    cloudinaryImageId,
+    promoted,
+  } = resData;
+
   return (
     <div className={style.card}>
       <div className={style["image-container"]}>
         <img
-          src="https://source.unsplash.com/430x250/?burger"
+          src={CDN_URL + cloudinaryImageId}
           alt="food"
           className={style.food}
           width={295}
         />
-        <p className={style.promoted}>Promoted</p>
+        {promoted ? <p className={style.promoted}>Promoted</p> : ""}
         <p className={style.offer}>50 % OFF up to 100</p>
-        <p className={style["delivery-time"]}>38 min</p>
+        <p className={style["delivery-time"]}>{deliveryTime} min</p>
       </div>
       <div className={style.content}>
-        <h3 className={style.restaurant}>Center Point Restaurant</h3>
-        <p className={style.cuisines}>North-Indian , Chinese</p>
+        <h3 className={style.restaurant}>{name}</h3>
+        <p className={style.cuisines}>{cuisines.join(", ")}</p>
+        {/* <div><div>{costForTwo}</div><div>{avgRating}</div></div> */}
 
         <hr />
         <div className={style["max-safety"]}>
