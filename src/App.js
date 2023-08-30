5b8361eefa6b70cd2cccff7a8913f04927c526a1
Neha <nehatualea@gmail.com>
2023-08-31 02:35:01 +0530

    definded : paths and implemented routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import Header from "./components/Header/Header";
-import Body from "./components/Body/Body";
+import AppLayout from "./App/AppLayout";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./Pages/About/About";
+import Body from "./components/Body/Body.js";
+import Error from "./Pages/Error/Error";
+import ResMenu from "./components/Restaurant/ResMenu";
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Body />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/restaurant/:resId",
+          element: <ResMenu />,
+        },
+      ],
+      errorElement: <Error />,
+    },
+  ]);
   return (
     <>
-      <Header />
-      <Body />
+      <RouterProvider router={router} />
     </>
   );
 };
