dd0cb0d5f87271fe6e9865e79962589afbe26e3c
Neha <nehatualea@gmail.com>
2023-09-07 22:45:55 +0530

    formatted

--- src/hooks/useRestaurantMenu.js
+++ src/hooks/useRestaurantMenu.js
@@ -1,36 +1,26 @@
-import React,{useState,useEffect} from 'react'
-import { RES_MENU_API } from '../utils/constants'
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { RES_MENU_API } from "../utils/constants";
+import { useNavigate } from "react-router-dom";
 
-const useRestaurantMenu=(restaurantId)=>{
+const useRestaurantMenu = (restaurantId) => {
+  const navigate = useNavigate();
+  const [resInfo, setResInfo] = useState(null);
 
- const navigate = useNavigate();
-    const [resInfo,setResInfo]=useState(null)
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const showMenu = (id) => {
+    console.log("clicked");
+    navigate("restaurant/" + id);
+  };
+  const fetchData = async () => {
+    const data = await fetch(RES_MENU_API + restaurantId);
+    const json = await data.json();
 
+    setResInfo(json.data);
+  };
 
-  useEffect(()=>{
-     fetchData();
-  },[])
-  const showMenu=(id)=>{
-    console.log("clicked")
-      navigate("restaurant/"+id)
- 
-     }
-const fetchData= async()=>{
-
-    const data = await fetch (RES_MENU_API +restaurantId);
-    const json= await data.json();
-
-    setResInfo(json.data)
-
-}
-
-return resInfo;
-
-
-}
+  return resInfo;
+};
 
 export default useRestaurantMenu;
-
-
-
