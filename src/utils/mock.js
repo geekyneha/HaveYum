c94b7c6e637623d229da1ca6a410c85cff7132b3
Neha <nehatualea@gmail.com>
2023-09-08 23:24:32 +0530

    Mock data from cuisines and offers

---
+++ src/utils/mock.js
@@ -0,0 +1,160 @@
+export const cuisines = [
+  {
+    id: 1,
+    name: "Biryani",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1675667625/PC_Creative%20refresh/Biryani_2.png",
+  },
+  {
+    id: 2,
+    name: "Dosa",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029850/PC_Creative%20refresh/3D_bau/banners_new/Dosa.png",
+  },
+  {
+    id: 3,
+    name: "North Indian",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1675667625/PC_Creative%20refresh/North_Indian_4.png",
+  },
+  {
+    id: 4,
+    name: "Paratha",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029853/PC_Creative%20refresh/3D_bau/banners_new/Paratha.png",
+  },
+  {
+    id: 5,
+    name: "Coffee",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029846/PC_Creative%20refresh/3D_bau/banners_new/Coffee.png",
+  },
+  {
+    id: 6,
+    name: "Pizza",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029856/PC_Creative%20refresh/3D_bau/banners_new/Pizza.png",
+  },
+  {
+    id: 7,
+    name: "Burger",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029845/PC_Creative%20refresh/3D_bau/banners_new/Burger.png",
+  },
+  {
+    id: 8,
+    name: "Khichdi",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029851/PC_Creative%20refresh/3D_bau/banners_new/Khichdi.png",
+  },
+  {
+    id: 9,
+    name: "Idli",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029846/PC_Creative%20refresh/3D_bau/banners_new/Idli.png",
+  },
+  {
+    id: 10,
+    name: "Chinese",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029848/PC_Creative%20refresh/3D_bau/banners_new/Chinese.png",
+  },
+  {
+    id: 11,
+    name: "Rolls",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029858/PC_Creative%20refresh/3D_bau/banners_new/Rolls.png",
+  },
+  {
+    id: 12,
+    name: "Pasta",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029854/PC_Creative%20refresh/3D_bau/banners_new/Pasta.png",
+  },
+  {
+    id: 13,
+    name: "Ice-cream",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029851/PC_Creative%20refresh/3D_bau/banners_new/Ice_Creams.png",
+  },
+  {
+    id: 14,
+    name: "Pav Bhaji",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029854/PC_Creative%20refresh/3D_bau/banners_new/Pav_Bhaji.png",
+  },
+  {
+    id: 15,
+    name: "Lassi",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029842/PC_Creative%20refresh/3D_bau/banners_new/Lassi.png",
+  },
+  {
+    id: 16,
+    name: "Vada",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029844/PC_Creative%20refresh/3D_bau/banners_new/Vada.png",
+  },
+  {
+    id: 17,
+    name: "South Indian",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1675667626/PC_Creative%20refresh/South_Indian_4.png",
+  },
+  {
+    id: 18,
+    name: "Vada Pav",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029843/PC_Creative%20refresh/3D_bau/banners_new/Vada_Pav.png",
+  },
+  {
+    id: 19,
+    name: "Desserts",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1675667630/PC_Creative%20refresh/Desserts_2.png",
+  },
+  {
+    id: 20,
+    name: "Poori",
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029855/PC_Creative%20refresh/3D_bau/banners_new/Poori.png",
+  },
+];
+
+export const offers = [
+  {
+    imgId: 1,
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/rng/md/carousel/production/0d50737c5d82b2e84c70d37dbbc0468d",
+  },
+  {
+    imgId: 2,
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/rng/md/carousel/production/280c841530518f3b04bb2fec89118e9f",
+  },
+  {
+    imgId: 3,
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/rng/md/carousel/production/6d7276d43a63da2cf051c014039ebf5b",
+  },
+  {
+    imgId: 4,
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/rng/md/carousel/production/930446fda5aed5c1ceac0f058971f851",
+  },
+  {
+    imgId: 5,
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/rng/md/carousel/production/baa850839548cde39dca259fc90c78e3",
+  },
+  {
+    imgId: 6,
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/rng/md/carousel/production/44af5c536bebce7d139a5ddbfdbb775c",
+  },
+  {
+    imgId: 7,
+    imgUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/rng/md/carousel/production/7dec1c62b4e539323ea54bae5e152c62",
+  },
+];
