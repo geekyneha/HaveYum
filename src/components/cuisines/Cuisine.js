6d5ddc9dddce1fd91779529aac405e6da9098d3b
Neha <nehatualea@gmail.com>
2023-09-08 23:15:22 +0530

    created: cuisines

---
+++ src/components/cuisines/Cuisine.js
@@ -0,0 +1,57 @@
+import React,{useRef} from 'react'
+import { cuisines } from '../../utils/mock'
+import style from './cuisine.module.css'
+import {BsFillArrowLeftCircleFill} from 'react-icons/bs'
+import {BsFillArrowRightCircleFill} from 'react-icons/bs'
+
+
+const Cuisine = () => {
+    const cuisineContainer=useRef(null)
+
+    const scrollLeft=()=>{
+        cuisineContainer.current.scrollBy({
+            left:-400,
+            behavior:"smooth"
+
+        })
+    }
+
+    const scrollRight=()=>{
+        cuisineContainer.current.scrollBy({
+            left:400,
+            behavior:"smooth"
+        })
+        
+
+    }
+  return (
+   <div className={style["cuisine-container"]}>
+
+   
+
+        <div className={style["heading"]}>
+            <h2>what's on your mind?</h2>
+            <div className={style["arrow-container"]}>
+                <BsFillArrowLeftCircleFill size={30} fill='lightgray' className={style["left-icon"]} onClick={scrollLeft} />
+                <BsFillArrowRightCircleFill size={30} fill='lightgray' className={style["right-icon"]} onClick={scrollRight} />
+
+            </div>
+        
+        
+        </div>
+       
+        <div className={style["cuisine"]} ref={cuisineContainer} >
+       { cuisines.map((cuisine)=>(<div  key={cuisine.id}>
+           <img src={cuisine.imgUrl} alt={cuisine.name} width={150} />
+           
+       </div>
+))
+
+       }
+       
+   </div>
+   </div>
+  )
+}
+
+export default Cuisine
