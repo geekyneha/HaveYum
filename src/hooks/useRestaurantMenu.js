b87c539ce2cf7f2dd551e0257d53a3ff835e9246
Neha <nehatualea@gmail.com>
2023-09-08 23:11:46 +0530

    created: custom Hook for fetching data

--- src/hooks/useRestaurantMenu.js
+++ src/hooks/useRestaurantMenu.js
@@ -1,20 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { RES_MENU_API } from "../utils/constants";
-import { useNavigate } from "react-router-dom";
 
-const useRestaurantMenu = (restaurantId) => {
-  const navigate = useNavigate();
+const useRestaurantMenu = (resId) => {
   const [resInfo, setResInfo] = useState(null);
 
   useEffect(() => {
     fetchData();
   }, []);
-  const showMenu = (id) => {
-    console.log("clicked");
-    navigate("restaurant/" + id);
-  };
+
   const fetchData = async () => {
-    const data = await fetch(RES_MENU_API + restaurantId);
+    const data = await fetch(RES_MENU_API + resId);
     const json = await data.json();
 
     setResInfo(json.data);
