522ac668398de4e06d09f8cfcb51321c2865ef28
Neha <nehatualea@gmail.com>
2023-09-09 20:31:27 +0530

    imported img from constants.js

--- src/Pages/Error/Error.js
+++ src/Pages/Error/Error.js
@@ -1,17 +1,14 @@
 import React from "react";
 import style from "./error.module.css";
 import { Link } from "react-router-dom";
+import { ERROR_GIF } from "../../utils/constants";
 
 const Error = () => {
   return (
     <div className={style["error-page"]}>
       <h1>Whoops! Nothing to see here ...</h1>
       <div>
-        {" "}
-        <img
-          src="https://cdn.svgator.com/images/2022/01/404-page-animation-example.gif"
-          alt="404-error"
-        />
+        <img src={ERROR_GIF} alt="404-error" />
       </div>
       <h3>sorry we cant find the page you're looking for</h3>
       <Link to="/">
