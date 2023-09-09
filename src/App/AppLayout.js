5e8ace2303917f7c23d1e69aed3a3eeb305e5db1
Neha <nehatualea@gmail.com>
2023-09-09 20:23:09 +0530

    Added: footer

--- src/App/AppLayout.js
+++ src/App/AppLayout.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
 
 const AppLayout = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };
