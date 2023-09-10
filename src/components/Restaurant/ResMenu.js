7f3f4acea13d40f5ee3d192c7b0c01c263ffca51
Neha <nehatualea@gmail.com>
2023-09-10 23:42:37 +0530

    changed : accordion

--- src/components/Restaurant/ResMenu.js
+++ src/components/Restaurant/ResMenu.js
@@ -1,19 +1,13 @@
-import  style from "./res-menu-.module.css";
+import style from "./res-menu-.module.css";
 import { useParams } from "react-router-dom";
 import { MdAccessTimeFilled } from "react-icons/md";
 import { MdOutlineDirectionsBike } from "react-icons/md";
 import { BiSolidOffer } from "react-icons/bi";
-import { BsCircleFill } from "react-icons/bs";
 import { HiOutlineCurrencyRupee } from "react-icons/hi";
 import { AiFillStar } from "react-icons/ai";
-import { GoTriangleUp } from "react-icons/go";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { CDN_URL } from "../../utils/constants.js";
 import { FSSAI_IMG } from "../../utils/constants.js";
 import useRestaurantMenu from "../../hooks/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
 
 const ResMenu = () => {
   const { resId } = useParams();
@@ -27,9 +21,18 @@ const ResMenu = () => {
     resInfo?.cards[0]?.card?.card?.info?.sla;
 
   const { offers } = resInfo?.cards[1]?.card?.card?.gridElements?.infoWithStyle;
-  const { cards } =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR ||
-    resInfo?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR;
+
+  const cardsArray =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards ||
+    resInfo?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+
+  const categories = cardsArray.filter(
+    (c) =>
+      c?.card?.card?.["@type"] ===
+      "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+  );
+
+  console.log(categories);
 
   return (
     <div className={style["menu-page"]}>
@@ -83,73 +86,12 @@ const ResMenu = () => {
           ))}
         </div>
         <div></div>
-        <div className={style["menu"]}>
-          {cards.slice(1, cards.length - 2).map((card, index) => (
-            <div key={index} className={style["accordion"]}>
-              <Accordion>
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
-                >
-                  <h4 className={style["category"]}>{card.card.card.title}</h4>
-                </AccordionSummary>
-                <AccordionDetails>
-                  {card.card.card.itemCards
-                    ? card.card.card.itemCards.map((item, index) => (
-                        <div className={style["dish-container"]} key={index}>
-                          <div>
-                            {item.card.info.isVeg === 1 ? (
-                              <div className={style["veg"]}>
-                                <BsCircleFill fill="green" size={10} />
-                              </div>
-                            ) : (
-                              <div className={style["non-veg"]}>
-                                <GoTriangleUp fill="red" />
-                              </div>
-                            )}
-                            <h4 className={style["dish"]}>
-                              {item.card.info.name}
-                            </h4>
-                            <h5 className={style["price"]}>
-                              ₹
-                              {item.card.info.defaultPrice / 100 ||
-                                item.card.info.price / 100}
-                            </h5>
-                            <p className={style["detail"]}>
-                              {item.card.info.description}
-                            </p>
-                          </div>
 
-                          <div>
-                            {item.card.info.imageId ? (
-                              <img
-                                src={CDN_URL + item.card.info.imageId}
-                                alt="dish"
-                                className={style["dish-image"]}
-                                width={110}
-                              />
-                            ) : (
-                              ""
-                            )}
-                            <div className={style["button-container"]}>
-                              {" "}
-                              <button
-                                className={
-                                  item.card.info.imageId
-                                    ? style["add-button"]
-                                    : style["add-btn"]
-                                }
-                              >
-                                ADD
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                      ))
-                    : ""}
-                </AccordionDetails>
-              </Accordion>
+        {/* catagories */}
+        <div className={style["menu"]}>
+          {categories.map((card, index) => (
+            <div key={index} className={style["accordion"]}>
+              <RestaurantCategory data={card?.card?.card} />
             </div>
           ))}
         </div>
