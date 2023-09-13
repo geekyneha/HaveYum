b87e680415e8290d295474d811779f8fc6620965
Neha <nehatualea@gmail.com>
2023-09-13 21:56:36 +0530

    connected store

--- src/App/AppLayout.js
+++ src/App/AppLayout.js
@@ -2,14 +2,18 @@ import React from "react";
 import Header from "../components/Header/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
+import { Provider } from "react-redux";
+import appStore from "../Redux/appStore";
 
 const AppLayout = () => {
   return (
-    <>
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
+    <Provider store={appStore}>
+      <>
+        <Header />
+        <Outlet />
+        <Footer />
+      </>
+    </Provider>
   );
 };
 
