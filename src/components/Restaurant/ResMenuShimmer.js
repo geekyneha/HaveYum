4f063266b6e44ca401e5483f51396946f64b12ae
Neha <nehatualea@gmail.com>
2023-09-17 22:19:17 +0530

    shimmer

---
+++ src/components/Restaurant/ResMenuShimmer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import style from './resmnu-shimmer.module.css'
+const ResMenuShimmer = () => {
+  return (
+    <div className={style["menu-page"]}>
+      <div className={style["menu-container"]}>
+     <div className={style["box-1"]}></div>
+     <div className={style["box-2"]}></div>
+     <div className={style["box-3"]}></div>
+     <div className={style["box-3"]}></div>
+     <div className={style["box-3"]}></div>
+     <div className={style["box-3"]}></div>
+     <div className={style["box-3"]}></div>
+     <div className={style["box-3"]}></div>
+     <div className={style["box-3"]}></div>
+     <div className={style["box-3"]}></div>
+     <div className={style["box-3"]}></div>
+     <div className={style["box-3"]}></div>
+     <div className={style["box-4"]}></div>
+      </div>
+    </div>
+  )
+}
+
+export default ResMenuShimmer
