e1d828ab5878297deadbcdc5ae445124b2dcab5b
Neha <nehatualea@gmail.com>
2023-09-17 22:14:02 +0530

    Removed : About

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,23 @@
-import React from "react";
-import * as style from "./header.module.css";
-import { FiShoppingBag } from "react-icons/fi";
-import { BiDish } from "react-icons/bi";
-import { MdPerson2 } from "react-icons/md";
-import { AiOutlineHome } from "react-icons/ai";
-import { TbListDetails } from "react-icons/tb";
-import { Link } from "react-router-dom";
-import { APP_LOGO } from "../../utils/constants";
-import { useSelector } from "react-redux";
+import React from 'react';
+import * as style from './header.module.css';
+import { FiShoppingBag } from 'react-icons/fi';
+import { BiDish } from 'react-icons/bi';
+import { MdPerson2 } from 'react-icons/md';
+import { AiOutlineHome } from 'react-icons/ai';
+import { TbListDetails } from 'react-icons/tb';
+import { Link } from 'react-router-dom';
+import { APP_LOGO } from '../../utils/constants';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
   const cart = useSelector((state) => state.cart.items);
   return (
-    <nav className={style["navbar"]}>
-      <div className={style["nav-container"]}>
-        <Link to="/" className={style["link"]}>
+    <nav className={style['navbar']}>
+      <div className={style['nav-container']}>
+        <Link
+          to="/"
+          className={style['link']}
+        >
           <div>
             <img
               src={APP_LOGO}
@@ -22,31 +25,37 @@ const Header = () => {
         yum"
               width={40}
               height={40}
-              className={style["logo"]}
+              className={style['logo']}
             />
           </div>
         </Link>
-        <div className={style["nav-items-container"]}>
-          <Link to="/" className={style["link"]}>
-            <li className={style["nav-item"]}>
-              <span className={style["d-none"]}> Home</span> <AiOutlineHome />
+        <div className={style['nav-items-container']}>
+          <Link
+            to="/"
+            className={style['link']}
+          >
+            <li className={style['nav-item']}>
+              <span className={style['d-none']}> Home</span> <AiOutlineHome />
             </li>
           </Link>
-          <li className={style["nav-item"]}>
+          {/* <li className={style["nav-item"]}>
             <span className={style["d-none"]}> About</span> <TbListDetails />
-          </li>
-          <li className={style["nav-item"]}>
-            <span className={style["d-none"]}> dish Craft</span> <BiDish />
+          </li> */}
+          <li className={style['nav-item']}>
+            <span className={style['d-none']}> dish Craft</span> <BiDish />
           </li>
 
-          <Link to="/cart" className={style["link"]}>
-            <li className={style["nav-item"]}>
-              <span className={style["d-none"]}>Cart </span> <FiShoppingBag />
-              <span className={style["item-count"]}>{cart.length}</span>
+          <Link
+            to="/cart"
+            className={style['link']}
+          >
+            <li className={style['nav-item']}>
+              <span className={style['d-none']}>Cart </span> <FiShoppingBag />
+              <span className={style['item-count']}>{cart.length}</span>
             </li>
           </Link>
-          <li className={style["nav-item"]}>
-            <span className={style["d-none"]}>Sign up</span> <MdPerson2 />
+          <li className={style['nav-item']}>
+            <span className={style['d-none']}>Sign up</span> <MdPerson2 />
           </li>
         </div>
       </div>
