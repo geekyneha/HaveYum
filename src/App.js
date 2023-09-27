573666c03ed3ec9fc9481583e12b8eab4db21b0d
Neha <nehatualea@gmail.com>
2023-09-27 18:54:43 +0530

    added: routes

--- src/App.js
+++ src/App.js
@@ -1,50 +1,54 @@
-import React, { lazy, Suspense } from "react";
-import AppLayout from "./App/AppLayout";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import About from "./Pages/About/About";
-import Body from "./components/Body/Body.js";
-import Error from "./Pages/Error/Error";
-import ResMenu from "./components/Restaurant/ResMenu";
-import Cart from "./components/Cart/Cart";
-import SignUp from "./components/Auth/Signup/SignUp";
-import Login from "./components/Auth/Login/Login";
+import React, { lazy, Suspense } from 'react';
+import AppLayout from './App/AppLayout';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from './Pages/About/About';
+import Body from './components/Body/Body.js';
+import Error from './Pages/Error/Error';
+import ResMenu from './components/Restaurant/ResMenu';
+import Cart from './components/Cart/Cart';
+import SignUp from './components/Auth/Signup/SignUp';
+import Login from './components/Auth/Login/Login';
+import Layout from './DishCraft/Layout/Layout';
 
 // const ResMenu = lazy(() => import("./components/Restaurant/ResMenu"));
 
 const App = () => {
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: '/',
       element: <AppLayout />,
       children: [
         {
-          path: "/",
+          path: '/',
           element: <Body />,
         },
         {
-          path: "/about",
+          path: '/about',
           element: <About />,
         },
         {
-          path: "/restaurant/:resId",
+          path: '/restaurant/:resId',
           element: <ResMenu />,
         },
         {
-          path: "/cart",
+          path: '/cart',
           element: <Cart />,
         },
       ],
       errorElement: <Error />,
-      
-
-    },{
-      path:"/login",
-      element:<Login />
     },
     {
-      path:"/signup",
-      element:<SignUp />
-    }
+      path: '/login',
+      element: <Login />,
+    },
+    {
+      path: '/signup',
+      element: <SignUp />,
+    },
+    {
+      path: '/dishcraft',
+      element: <Layout />,
+    },
   ]);
   return (
     <>
